abaab21e4411294c1958ef1200624efc1d61c4d5
velikodny <velikodny@gmail.com>
2016-03-16 23:44:58 +0300

    first restapi

---
+++ first.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/base64"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+type Message struct {
+	Address string `json:"address"`
+	City    string `json:"city"`
+	State   `json:"state"`
+	Zipcode int    `json:"zipcode"`
+	Country string `json:"country"`
+}
+
+type State struct {
+	Code string `json:"code"`
+	Name string `json:"name"`
+}
+
+type MsgErr struct {
+	Err string `json:"error"`
+}
+
+func sendResponse(w http.ResponseWriter, status int, msg interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	json.NewEncoder(w).Encode(msg)
+}
+
+const login = "demo:demo1"
+
+func main() {
+
+	StateName := map[string]string{
+		"fl": "Florida",
+	}
+
+	http.HandleFunc("/address/normalize", func(w http.ResponseWriter, r *http.Request) {
+		
+		//---------------------------------------
+
+		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted Area"`)
+
+		slice := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+		fmt.Println(len(slice))
+		if len(slice) != 2 {
+			http.Error(w, "Not authorized", 401)
+			return
+		}
+
+		encodelogin, err := base64.StdEncoding.DecodeString(slice[1])
+		if err != nil {
+			http.Error(w, err.Error(), 401)
+			return
+		}
+
+		if string(encodelogin) != login {
+			http.Error(w, "Not authorized", 401)
+			return
+		}
+
+		//---------------------------------------
+
+		query := r.URL.Query()
+
+		raw_address, ok := query["raw_address"]
+
+		if !ok {
+			sendResponse(w, 404, MsgErr{"raw_address required"})
+			return
+		}
+
+		part := strings.Split(raw_address[0], ",")
+
+		for i, elem := range part {
+			part[i] = strings.Trim(elem, " ")
+		}
+
+		if len(part) != 5 {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(MsgErr{"raw_address required 2"})
+			return
+		}
+
+		if len(part) == 5 {
+			code, _ := strconv.Atoi(part[3])
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			//w.Header().Set("Content-Type", "application/json; charset=utf-8")
+			msg := Message{part[0], part[1], State{part[2], StateName[strings.Trim(part[2], " ")]}, code, part[4]}
+			response, _ := json.Marshal(msg)
+			fmt.Fprintf(w, "%+v", string(response))
+		}
+
+	})
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
