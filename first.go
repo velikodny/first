01c86f332093510f12674ec9ac4a243c1310e812
Evgeny Velikodny <velikodny@gmail.com>
2016-03-26 23:06:52 +0300

    First RestAPI logging

--- first.go
+++ first.go
@@ -91,7 +91,9 @@ func main() {
 		//---------------------------------------
 
 		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted Area"`)
-
+        
+        log.Println("WWW-Authenticate")
+        
 		slice := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
 		fmt.Println(len(slice))
 		if len(slice) != 2 {
@@ -110,11 +112,15 @@ func main() {
 			return
 		}
 
+        log.Println("Authorized user")
+        
 		//---------------------------------------
 
 		query := r.URL.Query()
 
 		raw_address, ok := query["raw_address"]
+
+        log.Println(`Search "raw_address"`)
 
 		if !ok {
 			sendResponse(w, 404, MsgErr{"raw_address required"})
@@ -152,8 +158,11 @@ func main() {
 			json.NewEncoder(w).Encode(MsgErr{"raw_address required 2"})
 			return
 		}
+        
+        log.Println(`Found 5 part`)
 
 		if len(part) == 5 {
+            log.Println(`Сreate a message (json)`)
 			code, _ := strconv.Atoi(part[3])
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
@@ -161,9 +170,14 @@ func main() {
 			msg := Message{part[0], part[1], State{part[2], StateName[strings.Trim(part[2], " ")]}, code, part[4]}
 			response, _ := json.Marshal(msg)
 			fmt.Fprintf(w, "%+v", string(response))
-		}
+		} else {
+            log.Println(`The message (json) is not created`)    
+        }
+        
+        log.Println(`Тhe message (json) is created`)
 
 	})
+    log.Println("listen tcp localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 
 }
