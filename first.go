b03af22f7089fe6c7ed0d8e6822e9a21de3ff5c7
Evgeny Velikodny <velikodny@gmail.com>
2016-03-28 15:50:52 +0300

    First RestAPI

--- first.go
+++ first.go
@@ -3,8 +3,9 @@ package main
 import (
 	"encoding/base64"
 	"encoding/json"
+	"flag"
 	"fmt"
-	//"io."
+	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
@@ -81,7 +82,12 @@ func bypassResultGoogle(res []Results) Message {
 const login = "demo:demo1"
 
 func main() {
-	log.SetFlags(-1)
+
+	EnableLog := flag.Bool("log", false, "Enable log")
+	flag.Parse()
+	if !*EnableLog {
+		log.SetOutput(ioutil.Discard)
+	}
 
 	StateName := map[string]string{
 		"fl": "Florida",
@@ -98,17 +104,20 @@ func main() {
 		slice := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
 		fmt.Println(len(slice))
 		if len(slice) != 2 {
+			log.Println("Not authorized, http error 401")
 			http.Error(w, "Not authorized", 401)
 			return
 		}
 
 		encodelogin, err := base64.StdEncoding.DecodeString(slice[1])
 		if err != nil {
+			log.Println(err.Error())
 			http.Error(w, err.Error(), 401)
 			return
 		}
 
 		if string(encodelogin) != login {
+			log.Println("Bad login or passwrd")
 			http.Error(w, "Not authorized", 401)
 			return
 		}
@@ -119,29 +128,39 @@ func main() {
 
 		query := r.URL.Query()
 
-		raw_address, ok := query["raw_address"]
+        log.Print("Query: ")
+		log.Println(query)
 
 		log.Println(`Search "raw_address"`)
 
+		raw_address, ok := query["raw_address"]
+
 		if !ok {
+			log.Println(`"raw_address" not found`)
 			sendResponse(w, 404, MsgErr{"raw_address required"})
 			return
 		}
+
+		log.Println(`"raw_address" found`)
 
 		part := strings.Split(raw_address[0], ",")
 
 		//--------------------------------------
 		strAddress := strings.Replace(raw_address[0], " ", "+", -1)
 
+		log.Println("Address search in Google")
+        
+        log.Println("http.Get: ", "https://maps.googleapis.com/maps/api/geocode/json?address=", strAddress, "&key=AIzaSyC-OyuXWSaNdtjcCTC4oz7W1jxv5MwCP8k&language=en")
 		respGoogle, err := http.Get("https://maps.googleapis.com/maps/api/geocode/json?address=" + strAddress + "&key=AIzaSyC-OyuXWSaNdtjcCTC4oz7W1jxv5MwCP8k&language=en")
 
 		var resultGoogle GoogleResponse
 		err = json.NewDecoder(respGoogle.Body).Decode(&resultGoogle)
 		if err != nil {
+            log.Println(err.Error())
 			fmt.Fprintf(w, "Error: %v", err)
 		}
 
-		//msgPass := Message{}
+		log.Println("Address found")
 
 		findField := bypassResultGoogle(resultGoogle.Results)
 
@@ -167,7 +186,6 @@ func main() {
 			code, _ := strconv.Atoi(part[3])
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
-			//w.Header().Set("Content-Type", "application/json; charset=utf-8")
 			msg := Message{part[0], part[1], State{part[2], StateName[strings.Trim(part[2], " ")]}, code, part[4]}
 			response, _ := json.Marshal(msg)
 			fmt.Fprintf(w, "%+v", string(response))
